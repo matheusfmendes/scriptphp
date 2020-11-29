d06b1267a5ea45d9f101177e9436d7f4e23275e1
Matheus Mendes <50635097+matheusfmendes@users.noreply.github.com>
2020-11-29 03:08:45 -0300

    Add files via upload

---
+++ script.php
@@ -0,0 +1,13 @@
+4<?php
+
+$filename = "https://bc-v2.pressmatrix.com/pt-BR/profiles/c9e49eb97a66/editions/13e7845d8934afa67619/pages/toc";
+
+$content = file_get_contents($filename);
+
+$pattern = '/https:\/\/[\w-]+\.[\w-]+\.[\w-]+\/[\w-]+\.[\w-]+\.[\w-]+\.[\w-]+\/\w+\/[\w-]+\/[\w-]+\/[\w-]+\/\d+\/\d+\/\d+\/\w+\/\w+\/\w+\.jpg\?\d+/';
+
+preg_match_all($pattern, $content, $match);
+
+foreach ($match['0'] as $value) {
+    echo $value . "<br>";
+}
